37f2e793ddc6c603a528d8414ce654b0306c3872
StealThisNick <wozniakxyz@gmail.com>
2019-01-04 21:27:57 +0100

    Tune-up python script and readme

--- README.md
+++ README.md
@@ -1,31 +1,26 @@
 # Description:
-1. Testing task from intive, for patronage 2019 Machine Learning & Big Data
-    * zadanie1_cognitive.py, salary.csv
-2. Python script are taking .csv file, filter it from missing data and estimate salary, on colums 'workedYears' and 'salaryBrutto', and plot it on diagram.
+Testing task from [Intive](https://www.intive.com/), for [patronage](https://www.intive.com/en/insights/csr/the-patronage-program) 2019 Machine Learning & Big Data.
+    
+Python script (`zadanie1_cognitive.pu`) does:
+1. taking `salary.csv` file
+2. filter it from missing data
+3. estimate salary, on colums 'workedYears' and 'salaryBrutto'
+4. plot it on diagram
+5. prints additional estiamated values. 
 
 # Requirments:
-* python 3.x
+* python 3.x- it was tested on 3.6
 * pip
 
 # Instalation:
-1. Library instalation via. pip: pip install name_of_library
-    * ex: pip install pandas
-
+```bash
+$ pip install pandas matplotlib scipy statsmodels
+```
 # Usage:
-Input:
-    filtred_data.plot(
-                    kind='scatter',
-                    x='workedYears', y='salaryBrutto',
-                    label='actual salary')
-    plt.plot(
-        worked_years, salary_pred, c='red',
-        linewidth=3, label='predicted salary brutto')
-    plt.grid(True)
-    plt.legend()
-    plt.title('Salary over years')
-    plt.xlabel('Worked Years')
-    plt.ylabel('Salary Brutto')
-    plt.show()
+```bash
+$ python zadanie1_cognitive.py 
+Predicted salary brutto growth is: 695.85 per year, with error of: 0.13%
+```
 
 Predicted Output:
 
--- zadanie1_cognitive.py
+++ zadanie1_cognitive.py
@@ -4,59 +4,70 @@ import numpy as np
 import statsmodels.formula.api as smf
 from pathlib import Path
 
-salary_test = Path('salary.csv')
-if salary_test.exists():
-    try:
-        DEBUG = False
-        # Reading data with na_values=[' '] to treat empty
-        # string as missing value:
-        data = pd.read_csv('salary.csv', na_values=[' '])
-        filtred_data = data.copy()
-        # creating a copy of dataframe and filtered it of missing:
-        filtred_data = data[pd.notna(data['salaryBrutto'])]
-        # adding x and y for future plot:
-        worked_years = filtred_data['workedYears']
-        salary_brutto = filtred_data['salaryBrutto']
-        # start lineral regression via. salaryBrutto and workedYears:
-        model1 = smf.ols(formula='salaryBrutto~workedYears', data=filtred_data)
-        res = model1.fit(q=.5)
-        pred_value = res.params.get_values()
-        if DEBUG:
-            print(model1.params)  # getting alfa nad beta
-            print(model1.pvalues)  # checking p-values
-            print(model1.rsquared)  # checking r^2
-            print(model1.summary())  # check summary of data
+CSV_FILE = 'salary.csv'
+DEBUG = False
 
-        # predict salary brutto for each row:
-        salary_pred = res.predict(pd.DataFrame(filtred_data['workedYears']))
-        # plot actual salary and predicted salary:
-        filtred_data.plot(
-                        kind='scatter',
-                        x='workedYears', y='salaryBrutto',
-                        label='actual salary')
-        plt.plot(
-            worked_years, salary_pred, c='red',
-            linewidth=3, label='predicted salary brutto')
-        plt.grid(True)
-        plt.legend()
-        plt.title('Salary over years')
-        plt.xlabel('Worked Years')
-        plt.ylabel('Salary Brutto')
-        plt.show()
+salary_path = Path(CSV_FILE)
+# Checking if .csv file exist:
+while not salary_path.exists():
+    print(f'Path "{salary_path.as_posix()}" not exist')
+    new_csv = input("Type path to new csv file, or empty value for exit: ")
+    if not new_csv:
+        print('Bye!')
+        exit(1)
+    salary_path = Path(new_csv)
 
-        # adding predicted slary to dataframe:
-        filtred_data['salary_pred'] = pd.Series(
-            salary_pred, index=filtred_data.index)
-        # calculate Residual Standard Error:
-        x = (
-            (filtred_data.loc[:, 'salaryBrutto'] -
-                filtred_data.loc[:, 'salary_pred'])**2).tolist()
-        RSEd = sum(x)
-        RSE = np.sqrt(RSEd/45)
-        # mean of slaryBrutto nedded to calculate actual error of etimate:
-        salarymean = np.mean(filtred_data.loc[:, 'salaryBrutto'])
-        error = RSE/salarymean  # calculating error
-        print(f'Predicted salary brutto growth is: {round(pred_value[1],6)}', end=' '),
-        print(f'per year, with error of: {round(error,2)}%')
-    except:
-        print(f'File {salary_test}, dont exist or are in other path')
+try:
+
+    # Reading data with na_values=[' '] to treat empty
+    # string as missing value:
+    data = pd.read_csv(salary_path, na_values=[' '])
+    # creating a copy of dataframe and filtered it of missing:
+    filtred_data = data[pd.notna(data['salaryBrutto'])]
+    # adding x and y for future plot:
+    worked_years = filtred_data['workedYears']
+    salary_brutto = filtred_data['salaryBrutto']
+    # start lineral regression via. salaryBrutto and workedYears:
+    model1 = smf.ols(formula='salaryBrutto~workedYears', data=filtred_data)
+    res = model1.fit(q=.5)
+    pred_value = res.params.get_values()
+    if DEBUG:
+        print(res.params)  # getting alfa nad beta
+        print(res.pvalues)  # checking p-values
+        print(res.rsquared)  # checking r^2
+        print(res.summary())  # check summary of data
+
+    # predict salary brutto for each row:
+    salary_pred = res.predict(pd.DataFrame(worked_years))
+    # plot actual salary and predicted salary:
+    filtred_data.plot(
+                    kind='scatter',
+                    x='workedYears', y='salaryBrutto',
+                    label='actual salary')
+    plt.plot(
+        worked_years, salary_pred, c='red',
+        linewidth=3, label='predicted salary brutto')
+    plt.grid(True)
+    plt.legend()
+    plt.title('Salary over years')
+    plt.xlabel('Worked Years')
+    plt.ylabel('Salary Brutto')
+    plt.show()
+
+    # adding predicted slary to dataframe:
+    filtred_data = filtred_data.assign(
+        salary_pred=pd.Series(salary_pred).values)
+    # calculate Residual Standard Error:
+    x = (
+        (filtred_data.loc[:, 'salaryBrutto'] -
+            filtred_data.loc[:, 'salary_pred'])**2).tolist()
+    RSEd = sum(x)
+    RSE = np.sqrt(RSEd/45)
+    # mean of slaryBrutto nedded to calculate actual error of etimate:
+    salarymean = np.mean(filtred_data.loc[:, 'salaryBrutto'])
+    error = RSE/salarymean  # calculating error
+    print(f'Predicted salary brutto growth is: {round(pred_value[1],2)}', end=' '),
+    print(f'per year, with error of: {round(error,2)}%')
+except Exception as exc:
+    print(exc)
+    exit(1)
